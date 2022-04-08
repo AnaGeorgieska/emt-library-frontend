d9d992b7760ff857c9397fb0362dc63e28a73d18
AnaGeorgieska <68482597+AnaGeorgieska@users.noreply.github.com>
2022-04-09 00:28:29 +0200

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,114 @@
+import logo from '../../logo.svg';
+import './App.css';
+import React from "react";
+import { Routes, Route, useParams, BrowserRouter, Navigate } from "react-router-dom";
+import {Component} from "react";
+import Books from '../Books/BookList/books';
+import Categories from '../Categories/categories';
+import Authors from '../Authors/authors';
+import BookAdd from '../Books/BookAdd/bookAdd';
+import BooksService from "../../repository/booksRepository";
+import Header from "../Header/header";
+import BookEdit from "../Books/BookEdit/bookEdit";
+
+class App extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            books: [],
+            categories: [],
+            authors: [],
+            selectedBook: {}
+        }
+    }
+    render() {
+        return(
+
+            <BrowserRouter>
+                <Header/>
+                <Routes>
+                    <Route path="/books/add" element={<BookAdd categories={this.state.categories}
+                          authors={this.state.authors} onAddBook={this.addBook}/>} />
+                    <Route path="/books/edit/:id" element={<BookEdit categories={this.state.categories}
+                          authors={this.state.authors} onEditBook={this.editBook} book={this.state.selectedBook}/>} />
+                    <Route path="/books" element={<Books books={this.state.books} onDelete={this.deleteBook} onEdit={this.getBook}
+                                                         mark={this.markBook} />} />
+                    <Route path="/categories" element={<Categories categories={this.state.categories} />}/>
+                    <Route path="/authors" element={<Authors authors={this.state.authors} />}/>
+                    {/*<Navigate to={"/books"}/>*/}
+                </Routes>
+            </BrowserRouter>
+        )
+    }
+
+    loadBooks = () =>{
+        BooksService.fetchBooks()
+            .then((data) => {
+                this.setState({
+                    books: data.data
+                })
+            })
+    }
+
+    loadCategories = () => {
+        BooksService.fetchCategories()
+            .then((data) => {
+                this.setState({
+                    categories: data.data
+                })
+            })
+    }
+
+    loadAuthors = () => {
+        BooksService.fetchAuthors()
+            .then((data) => {
+                this.setState({
+                    authors: data.data
+                })
+            })
+    }
+
+    deleteBook = (id) => {
+        BooksService.deleteBook(id)
+            .then(() => {
+                this.loadBooks();
+            })
+    }
+    markBook = (id) => {
+        BooksService.markBook(id)
+            .then(() => {
+                this.loadBooks();
+            })
+    }
+    addBook = (name, category, author, availableCopies) => {
+        BooksService.addBook(name, category, author, availableCopies)
+            .then(() => {
+                this.loadBooks();
+            })
+    }
+
+    getBook = (id) => {
+        debugger
+        BooksService.getBook(id)
+            .then((data) => {
+                this.setState({
+                    selectedBook: data.data
+                })
+            })
+    }
+
+    editBook = (id, name, category, author, availableCopies) => {
+        BooksService.editBook(id, name, category, author, availableCopies)
+            .then(() => {
+                this.loadBooks();
+            })
+    }
+
+    componentDidMount() {
+        this.loadBooks();
+        this.loadCategories();
+        this.loadAuthors();
+    }
+}
+
+export default App;
---
+++ src/components/Books/BookTerm/bookTerm.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+const bookTerm = (props) => {
+    return(
+    <tr>
+        <td scope={"col"}>{props.term.name}</td>
+        <td scope={"col"}>{props.term.category}</td>
+        <td scope={"col"}>{props.term.author.name}</td>
+        <td scope={"col"}>{props.term.availableCopies}</td>
+        <td scope={"col"} className={"text-right"}>
+            <a title={"Delete"} className={"btn btn-danger mx-2"}
+                onClick={() => props.onDelete(props.term.id)}
+            >Delete
+            </a>
+            <Link className={"btn btn-info mx-2"}
+                onClick={()=>props.onEdit(props.term.id)}
+                  to={`/books/edit/${props.term.id}`}>
+                Edit
+            </Link>
+            <a title={"Mark as taken"} className={"btn btn-success mx-2"}
+               onClick={() => props.onMarkAsTaken(props.term.id)}
+            >Mark as taken
+            </a>
+        </td>
+    </tr>
+
+    )
+}
+export default bookTerm;
